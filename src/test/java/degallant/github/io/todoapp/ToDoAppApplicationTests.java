59aa07392130019ef840d60aa1ec4cb7ea6fbdbc
Artenes Nogueira <artenesama@gmail.com>
2023-01-14 15:56:35 -0400

    Add some more notes

--- src/test/java/degallant/github/io/todoapp/ToDoAppApplicationTests.java
+++ src/test/java/degallant/github/io/todoapp/ToDoAppApplicationTests.java
@@ -540,11 +540,37 @@ class ToDoAppApplicationTests {
 
     //todo extract authenticate user for easier access
 
+    //todo add proper exception handling
+
+    //todo deal with internationalization
+
+    //todo deal with users roles
+
+    //todo check about caching - Cache-Control header
+
+    //todo deal with rate limit
+
+    //todo deal with logginh and monitoring
+
+    //todo check about graphql
+
+    //todo get back to OpenAPI/Swagger
+
+    //todo check about CORS
+
     //about date time in java https://reflectoring.io/spring-timezones/
 
     //retardedly, you can't have enums in the database and bring them to hibernate, maybe this can help: https://docs.jboss.org/hibernate/orm/6.1/userguide/html_single/Hibernate_User_Guide.html#basic-enums
 
     //when creating queries in a repository, by default it uses JPQL
+
+    //top 10 security issues with rest apis https://github.com/OWASP/API-Security
+
+    //api versioning https://www.troyhunt.com/your-api-versioning-is-wrong-which-is/
+
+    //HATEOAS https://stateless.co/hal_specification.html https://restfulapi.net/hateoas/
+
+    //API paging https://www.mixmax.com/engineering/api-paging-built-the-right-way
 
     @AfterEach
     public void tearDown() {
